925d015e71c34fe904d9bf3e83a22e724e75ba4e
Sam Gentry <gensa844@student.otago.ac.nz>
2021-04-15 15:01:51 +1200

    added data record scene

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText : MonoBehaviour
+{
+    private GameObject cameraObj;
+    private string camPos;
+    public bool found = false;
+    public string allCamPos;
+    private string camPosX;
+    public string allCamPosX;
+    private string camPosY;
+    public string allCamPosY;
+    private string camPosZ;
+    public string allCamPosZ;
+    public string allCamPosCsv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObj = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camPos = cameraObj.transform.position.ToString();
+        camPosX = cameraObj.transform.position.x.ToString();
+        camPosY = cameraObj.transform.position.y.ToString();
+        camPosZ = cameraObj.transform.position.z.ToString();
+        allCamPos += (camPos+"\n");
+        allCamPosX += (camPosX + "\n");
+        allCamPosY += (camPosY + "\n");
+        allCamPosZ += (camPosZ + "\n");
+        allCamPosCsv += (camPosX + "," + camPosY + "," +camPosZ + ",\n");
+
+        this.gameObject.GetComponent<TextMesh>().text = (camPos + "\nFound = "+ found);
+
+    }
+
+    public void Demo()
+    {
+        found = true;
+    }
+
+    public void ButtonTest()
+    {
+        //Debug.Log("button clicked");
+        this.gameObject.GetComponent<RecordData>().WriteData("test.txt", allCamPos);
+        this.gameObject.GetComponent<RecordData>().WriteData("testX.txt", allCamPosX);
+        this.gameObject.GetComponent<RecordData>().WriteData("testY.txt", allCamPosY);
+        this.gameObject.GetComponent<RecordData>().WriteData("testZ.txt", allCamPosZ);
+        this.gameObject.GetComponent<RecordData>().WriteData("test.csv", allCamPosCsv);
+     //+ allCamPosZ, + allCamPosZ + "\n");   
+    }
+}
--- Assets/Scripts/RecordMove.cs
+++ Assets/Scripts/RecordMove.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class RecordMove : MonoBehaviour
 {
+    public GameObject textObj;
+
+    private float testNum = 0;
+
+    private Vector3 camAngle;
+    
+
+    private string camPosX;
+    private string camPosY;
+    private string camPosZ;
+    private string camRotX;
+    private string camRotY;
+    private string camRotZ;
+
+    private string allCamPosCsv;
+
+    private bool testing = false;
+    private float timer = 0.0f;
+    private float waitTime = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+        if (timer <= waitTime & testing == true)
+        {
+            //get positions and rotations and add to string
+            camPosX = this.gameObject.transform.position.x.ToString();
+            camPosY = this.gameObject.transform.position.y.ToString();
+            camPosZ = this.gameObject.transform.position.z.ToString();
+            camAngle = this.gameObject.transform.rotation.eulerAngles;
+            camRotX = camAngle.x.ToString();
+            camRotY = camAngle.y.ToString();
+            camRotZ = camAngle.z.ToString();
+            allCamPosCsv += (timer.ToString() + "," + camPosX + "," + camPosY + "," + camPosZ + "," + camRotX + "," + camRotY + "," + camRotZ + ",\n");
+
+            textObj.GetComponent<TextMesh>().text = (timer.ToString());
+        }
+        else if (timer > waitTime & testing == true)
+        {
+            this.gameObject.GetComponent<RecordData>().WriteData("HeadsetPose" + testNum + ".csv", allCamPosCsv);
+            allCamPosCsv = " ";
+
+            testing = false;
+        }
+        else
+        {
+            timer = 0.0f;
+        }
     }
+
+    public void Test()
+    {
+        //Debug.Log("button clicked");
+        testNum += 1;
+        testing = true;
+
+    }
+
 }
