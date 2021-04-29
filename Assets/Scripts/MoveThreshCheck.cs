8e2802b29386087163dc8f64bf328fa866fb299c
Karl Shipley <SHIKA823@student.otago.ac.nz>
2021-04-29 15:15:35 +1200

    Added example of how movement method works (No real threshhold values)

--- Assets/Scripts/MoveThreshCheck.cs
+++ Assets/Scripts/MoveThreshCheck.cs
@@ -8,9 +8,40 @@ public class MoveThreshCheck
     public float walking;
     public float jogging;
     public float sitUps;
+    public float xPos;
+    public float yPos;
+    public float zPos;
 
-    public string FindMovement(float[] poses)
+    public string FindMovement(float[] poses, int poseCount)
     {
-        if()
+        for (int j = 0; j <= poseCount - 1; j++)
+        {
+            xPos += poses[j].position.x;
+            yPos += poses[j].position.y;
+            zPos += poses[j].position.z
+
+        }
+
+        xPos = (xPos / poseCount);
+        yPos = (yPos / poseCount);
+        zPos = (zPos / poseCount);
+
+        if(xPos <= standStill)
+        {
+            return "Standing Still";
+
+        }else if( xPos >= walking && xPos < jogging)
+
+        {
+            return "Walking";
+
+        }else if(xPos >= jogging)
+
+        {
+            return "Jogging"
+        }
+
+
+ 
     }
 }
