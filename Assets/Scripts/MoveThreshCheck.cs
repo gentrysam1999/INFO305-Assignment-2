1bc0ece26eb2d07f9119fb848b3e0df40b31dac8
Karl Shipley <SHIKA823@student.otago.ac.nz>
2021-04-29 16:09:58 +1200

    added rotations pos

--- Assets/Scripts/MoveThreshCheck.cs
+++ Assets/Scripts/MoveThreshCheck.cs
@@ -11,6 +11,9 @@ public class MoveThreshCheck : MonoBehaviour
     public float xPos;
     public float yPos;
     public float zPos;
+    public float xRot;
+    public float yRot;
+    public float zRot;
 
     public string findMovement(List<Pose> poses, int poseCount)
     {
@@ -19,12 +22,18 @@ public class MoveThreshCheck : MonoBehaviour
             xPos += poses[j].position.x;
             yPos += poses[j].position.y;
             zPos += poses[j].position.z;
+            xRot += poses[j].position.x.eulerAngles;
+            yRot += poses[j].position.y.eulerAngles;
+            zRot += poses[j].position.z.eulerAngles;
 
         }
 
         xPos = (xPos / poseCount);
         yPos = (yPos / poseCount);
         zPos = (zPos / poseCount);
+        xRot = (xRot / poseCount);
+        yRot = (yRot / poseCount);
+        zRot = (zRot / poseCount);
 
         if (xPos <= standStill)
         {
